b12f21c2e13dc0fbd9c9464cdfb3629d6ae6e5fa
hjf <hjf@hjf.com.ar>
2020-09-12 00:08:58 -0300

    convert everything to Standard JS style

--- index.js
+++ index.js
@@ -5,37 +5,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-//we use JSON to pass data around
-app.use(express.json());
+// we use JSON to pass data around
+app.use(express.json())
 
 app.get('/dequeue', async (req, res) => {
-    let element = wonderq.dequeue();
-    if (element == null) {
-        res.status(204).send()
-        return
-    }
-    res.json(element)
+  const element = wonderq.dequeue()
+  if (element == null) {
+    res.status(204).send()
+    return
+  }
+  res.json(element)
 })
 
 app.post('/enqueue', async (req, res) => {
-    let element_id = wonderq.enqueue(req.body);
-    res.json({ "element_id": element_id })
+  const elementId = wonderq.enqueue(req.body)
+  res.json({ element_id: elementId })
 })
 
 app.get('/notifyDone', async (req, res) => {
-    let element_id = req.query["element_id"];
-    if (element_id === undefined) {
-        res.status(400).send("Bad request.")
-    }
+  const elementId = req.query.element_id
+  if (elementId === undefined) {
+    res.status(400).send('Bad request.')
+  }
 
-    if (wonderq.notifyDone(element_id)) {
-        res.status(204).send()
-    } else {
-        res.status(404).send("No such element in the queue.")
-    }
-
+  if (wonderq.notifyDone(elementId)) {
+    res.status(204).send()
+  } else {
+    res.status(404).send('No such element in the queue.')
+  }
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+  console.log(`Example app listening at http://localhost:${port}`)
 })
